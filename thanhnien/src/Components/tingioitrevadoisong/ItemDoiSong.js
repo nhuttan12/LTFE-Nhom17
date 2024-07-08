d60967c08302474b050740db67a41a0db55d4af2
PhamPhiTruong <21130593@st.hcmuaf.edu.vn>
2024-07-08 15:14:49 +0700

    Chỉnh sửa các file Item #5 #6 #7

--- thanhnien/src/Components/tingioitrevadoisong/ItemDoiSong.js
+++ thanhnien/src/Components/tingioitrevadoisong/ItemDoiSong.js
@@ -4,7 +4,7 @@ const ItemDoiSong = ({image, title, description, className}) =>{
     return (
         <div className={`life-item ${className}`}>
             <img src={image} alt={title} className='life-item-image'/>
-            <p className='life-item-description'>{description}</p>
+            <a className='life-item-description'>{description}</a>
         </div>
     );
 };
