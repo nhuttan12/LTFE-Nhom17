1fdc3c3b2cacc051790b8fd9173166c2c31616dc
Minh Thinh <21130549@st.hcmuaf.edu.vn>
2024-07-09 16:20:24 +0700

    css description

--- thanhnien/src/index.js
+++ thanhnien/src/index.js
@@ -16,12 +16,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/*<ListThiTruong/>*/}
-    {/* <ListTongHop/> */}
-    <Header/>
+     <ListTongHop/>
+    {/*<Header/>*/}
     {/* <ListGioiTreVaDoiSong/> */}
     {/* <ListTinNhanh360/> */}
     {/* <List24h/> */}
-    <DanhSachBaiBao/>
+    {/*<DanhSachBaiBao/>*/}
     {/* <Footer/> */}
     {/* <App/> */}
     {/* <DanhSachBaiBao/> */}
